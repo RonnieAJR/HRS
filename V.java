9347ddfa43147d402007ffb105e993849db5a334
lanceferr <lanceferrer1122@gmail.com>
2024-07-25 01:06:29 +0800

    bug fixes for setters of card 3_2, initCard3_3, setters for card 3_3

--- V.java
+++ V.java
@@ -30,6 +30,9 @@ public class V{
 	private JTextField txtTotalRoomAvail, txtTotalRoomReserved;
 
 	// card 3.3 view room info
+	private JComboBox<String> cmbxSlctRoom;
+	private JTextField roomName, floor, pricePerNight;
+	private JComboBox<Integer> cmbxDayAvail, cmbxDayReserved;
 
 	// card 3.4 view reservation info
 	
@@ -143,6 +146,8 @@ public class V{
 		this.txtEstimatedEarnings = new JTextField();
 		this.txtEstimatedEarnings.setEditable(false);
 
+		card3_1.add(new JLabel("Hotel Info"));
+
 		card3_1.add(new JLabel("Hotel Name: "));
 		card3_1.add(this.txtHotelName);
 
@@ -194,6 +199,39 @@ public class V{
 
 		this.cards.add(card3_2, "View Day Availability");
 
+	}
+
+	public void initCard3_3(){
+		JPanel card3_3 = new JPanel();
+
+		this.cmbxSlctRoom = new JComboBox<>();
+		this.roomName = new JTextField();
+		this.floor = new JTextField();
+		this.pricePerNight = new JTextField();
+		this.cmbxDayAvail = new JComboBox<>();
+		this.cmbxDayReserved = new JComboBox<>();
+
+		card3_3.add(new JLabel("Room Info"));
+
+		card3_3.add(new JLabel("Select Room"));
+		card3_3.add(this.cmbxSlctRoom);
+
+		card3_3.add(new JLabel("Room Name:"));
+		card3_3.add(this.roomName);
+
+		card3_3.add(new JLabel("Room Floor:"));
+		card3_3.add(this.floor);
+
+		card3_3.add(new JLabel("Room Price Per Night:"));
+		card3_3.add(this.pricePerNight);
+
+		card3_3.add(new JLabel("Days Available:"));
+		card3_3.add(this.cmbxDayAvail);
+
+		card3_3.add(new JLabel("Days Reserved"));
+		card3_3.add(this.cmbxDayReserved);
+
+		this.cards.add(card3_3, "View Room");
 	}
 		
 	
@@ -331,12 +369,42 @@ public class V{
 	}
 
 	//card 3_2 setters
-	public void setRoomAvailable(String[] roomAvailable){
-		this.roomAvailable = roomAvailable;
+	public void setRoomAvailable(String[] roomsAvailable){
+		for(String roomAvail: roomsAvailable)
+			this.cmbxAvail.addItem(roomAvail);
 	}
 
-	public void setRoomReserved(String[] roomReserved){
-		this.roomReserved = roomReserved;
+	public void setRoomReserved(String[] roomsReserved){
+		for(String roomRes: roomsReserved)
+			this.cmbxReserved.addItem(roomRes);
+	}
+
+	//card 3_3 setters
+	public void setCmbxSlctRoom(String[] roomNames){
+		for(String roomName: roomNames)
+			this.cmbxSlctRoom.addItem(roomName);
+	}
+
+	public void setRoomName(String roomName){
+		this.roomName.setText(roomName);
+	}
+
+	public void setRoomFloor(char floor){
+		this.floor.setText("" + floor);
+	}
+
+	public void setPricePerNight(double price){
+		this.pricePerNight.setText("" + price);
+	}
+
+	public void setCmbxDayAvail(Integer[] dayAvail){
+		for(Integer day: dayAvail)
+			this.cmbxDayAvail.addItem(day);
+	}
+
+	public void setCmbxDayReserved(Integer[] dayReserved){
+		for(Integer day: dayReserved)
+			this.cmbxDayReserved.addItem(day);
 	}
 
 	// card 4
