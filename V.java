ca21a36786b2e8032821c4586c0862c44ad53c26
RonnieAJR <ronnieabiogjr@gmail.com>
2024-07-24 16:58:17 +0800

    for merging card 4

--- V.java
+++ V.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.awt.event.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class V{
 	private JFrame frame; // borderlayout
@@ -22,7 +23,7 @@ public class V{
 	// card 3.4 view reservation info
 	
 	// card 4 manage hotel
-	private JButton btnRnmHtl, btnAddRoom, btnRmvRoom, btnModPrice, btnUpdPrice, btnRmvRsrv, btnRmvHtl
+	private JButton btnRnmHtl, btnAddRoom, btnRmvRoom, btnModPrice, btnUpdPrice, btnRmvRsrv, btnRmvHtl;
 	private JTextField txtPriceMod;
 	
 	// card 4.1 rename hotel
@@ -33,7 +34,7 @@ public class V{
 	// card 4.6 remove reservation
 	// card 4.7 remove hotel
 	
-	private JComboBox cmbxHtls, cmbxDays, cmbxRooms, cmbxRsrvs;
+	private JComboBox<String> cmbxHtls, cmbxDays, cmbxRooms, cmbxRsrvs;
 	private JLabel lblFeedback;
 	
 	
@@ -48,6 +49,38 @@ public class V{
 	}
 	
 	public void init(){
-		new JPanel(new CardLayout());
+		this.cards = new JPanel(new CardLayout());
+		
+		// initialize gui here
+		
+		JPanel card4 = new JPanel(new GridLayout(8, 1, 0, 5));
+		
+		String[] hotels = {"Hotel1", "Hotel2", "Hotel3"};
+		this.cmbxHtls = new JComboBox<String>(hotels); // this is local
+		this.cmbxHtls.setEditable(false);
+		
+		this.btnRnmHtl = new JButton("Rename Hotel");
+		this.btnAddRoom = new JButton("Add Room");
+		this.btnRmvRoom = new JButton("Remove Room");
+		this.btnModPrice = new JButton("Modify Day Price");
+		this.btnUpdPrice = new JButton("Update Price");
+		this.btnRmvRsrv = new JButton("Remove Reservation");
+		this.btnRmvHtl = new JButton("Remove Hotel");
+		
+		card4.add(cmbxHtls);
+		card4.add(btnRnmHtl);
+		card4.add(btnAddRoom);
+		card4.add(btnRmvRoom);
+		card4.add(btnModPrice);
+		card4.add(btnUpdPrice);
+		card4.add(btnRmvRsrv);
+		card4.add(btnRmvHtl);
+		
+		this.cards.add(card4);
+		this.frame.add(cards, BorderLayout.CENTER);
+	}
+	
+	public static void main(String args[]){
+		new V();
 	}
 }
