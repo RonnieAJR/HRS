8bf8a2b928e31e932c33660ebbdd42df3b5f349b
lanceferr <lanceferrer1122@gmail.com>
2024-07-26 20:41:21 +0800

    bug fixes

--- V_Card4_5.java
+++ V_Card4_5.java
@@ -4,27 +4,27 @@ import java.awt.event.ActionListener;
 public class V_Card4_5 {
     private JPanel cards;
     
-    private JTextField txtPrcMod, txtCnfrm;
-    private JButton btnSbmt, btnMngHtlBck;
+    private JTextField txtUpdtNew, txtCnfrm;
+    private JButton btnUpdt, btnMngHtlBck;
     private JLabel txtFdbck;
 
     public V_Card4_5(JPanel cards){
         this.cards = cards;
         JPanel card4_5 = new JPanel();
         
-        this.txtPrcMod = new JTextField(10);
+        this.txtUpdtNew = new JTextField(10);
         this.txtCnfrm = new JTextField();
 
-        this.btnSbmt = new JButton("Submit");
+        this.btnUpdt = new JButton("Update");
         this.btnMngHtlBck = new JButton("Back to Manage Hotel");
 
         this.txtFdbck = new JLabel();
 
 
         card4_5.add(new JLabel("Input new price"));
-        card4_5.add(this.txtPrcMod);
+        card4_5.add(this.txtUpdtNew);
 
-        card4_5.add(this.btnSbmt);
+        card4_5.add(this.btnUpdt);
         card4_5.add(this.btnMngHtlBck);
         card4_5.add(this.txtCnfrm);
         card4_5.add(this.txtFdbck);
@@ -32,19 +32,25 @@ public class V_Card4_5 {
         this.cards.add(card4_5, "Update Hotel Price");
     }
 
-    public double getTxtPrcMod(){return Double.parseDouble(txtPrcMod.getText());}
+    public double getTxtUpdtNew(){return Double.parseDouble(txtUpdtNew.getText());}
 
-    public void resetTxtPrcMod(){this.txtPrcMod.setText("");}
-
-    public void resetTxtConfirm(){this.txtCnfrm.setText("");}
-
-    public void resetTxtFdbck(){this.txtFdbck.setText("");}
+    public void resetUpdtHtlPrc(){
+        this.txtUpdtNew.setText("");
+        this.txtCnfrm.setText("");
+        this.txtFdbck.setText("");
+    }
 
     public void setTxtConfirm(String confirm){this.txtCnfrm.setText(confirm);}
 
     public void setTxtFdbck(String feedback){this.txtFdbck.setText(feedback);}
 
-    public void setBtnSbmtListener(ActionListener al){this.btnSbmt.addActionListener(al);}
+    public void setBtnUpdtListener(ActionListener al){
+        if(this.btnUpdt.getActionListeners().length == 0)
+            this.btnUpdt.addActionListener(al);
+    }
 
-    public void setBtnMngHtlBckListener(ActionListener al){this.btnMngHtlBck.addActionListener(al);}
+    public void setBtnMngHtlBckListener(ActionListener al){
+        if(this.btnMngHtlBck.getActionListeners().length == 0)
+            this.btnMngHtlBck.addActionListener(al);
+    }
 }
